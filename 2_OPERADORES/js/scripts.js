078dec091430cd866124891039f46081f2bdc865
Carolina Teixeira <carolinateixeira.150@gmail.com>
2025-05-21 21:19:01 -0300

    feat

---
+++ 2_OPERADORES/js/scripts.js
@@ -0,0 +1,45 @@
+// 8 - Boolean
+// console.log(true);
+// console.log(5 > 20);
+// console.log(30 > 10);
+// console.log(55 < 23);
+// console.log(typeof false);
+
+// 9 - Comparações
+// console.log(5 <= 5);
+// console.log(5 >= 6);
+// console.log(9 > 10);
+// console.log(4 == 4.1);
+// console.log(7 === 7);
+
+// 10 - Idêntico
+// console.log(9 == "9");
+// console.log(9 + "9");
+// console.log(9 === "9");
+// console.log(9 != "9");
+// console.log(9 !== "9");
+
+// 11 - Operadores lógicos
+// console.log(true && true);
+// console.log(true && false);
+// console.log(5 > 2 && 2 < 10);
+// console.log(5 > 2 && "Carol" === 1);
+// console.log(5 > 2 || "Carol" === 1);
+// console.log(5 > 2 || 5 > 100);
+// console.log(!true);
+// console.log(!5 > 2); 
+/*O operador ! (negação lógica) transforma qualquer valor truthy em false. E false é convertido em 0, então fica 0 > 2 que resulta em FALSE */
+
+// 12 - empty values
+
+// console.log(typeof null, typeof undefined);
+// console.log(null === undefined);
+// console.log(null == undefined);
+// console.log(null == false);
+// console.log(undefined == false);
+
+// 13 - mudança de tipos
+console.log(5 * null);
+console.log("5" - 3);
+console.log("5" + 1);
+console.log("a" * "b");
