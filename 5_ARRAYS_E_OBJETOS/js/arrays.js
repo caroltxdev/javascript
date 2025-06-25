0ac34ffe6c52616e248b621a6ec902e93afe2670
Carolina Teixeira <carolinateixeira.150@gmail.com>
2025-06-25 07:22:23 -0300

    feat

--- 5_ARRAYS_E_OBJETOS/js/arrays.js
+++ 5_ARRAYS_E_OBJETOS/js/arrays.js
@@ -95,3 +95,80 @@ const users = ["Matheus", "João", "Pedro", "Miguel"];
 for(let i = 0; i < users.length; i++) {
     console.log(`Listando o usuário: ${users[i]}`);
 }
+// 11 - push e pop
+const array = ["a", "b", "c"]
+console.log(array);
+
+array.push("d");
+console.log(array);
+console.log(array.length);
+
+array.pop();
+console.log(array);
+
+array.pop(); //A cada pop ele vai deletando um, o push e pop vai do ÚLTIMO ao primeiro
+console.log(array);
+
+// 12 - shift e unshift
+const letters = ["a", "b", "c"]
+const letter = letters.shift();
+console.log(letter);
+console.log(letters);
+//No shift e unshift eles add e deletam a partir do PRIMEIRO
+letters.unshift("p", "q", "r");
+letters.unshift("z");
+console.log(letters);
+
+// 13 - indexOf e lastIndexOf
+const myElements = ["Morango", "Maça", "Abacate", "Pêra", "Uva verde"];
+
+console.log(myElements.indexOf("Maça"));
+console.log(myElements.indexOf("Pêra"));
+//o indexOf e lastIndezOf mostra o índice que eu escolher
+console.log(myElements[2]);
+console.log(myElements[myElements.indexOf("Abacate")]);
+
+console.log(myElements.lastIndexOf("Uva verde"));
+console.log(myElements.indexOf("Uva verde"));
+
+console.log(myElements.indexOf("Mamão"));//quando não existe, ele retorna -1
+
+// 14 - slice
+const testeSlice = ["a", "b", "c", "d", "e", "f"]
+
+const subArray = testeSlice.slice(2, 4); //Mostra só o "c" e o "e"
+console.log(subArray); // Mostra só o 2 e 4
+console.log(testeSlice);//mostra todos os vetores
+
+const subArray2 = testeSlice.slice(2, 4 + 1);
+console.log(subArray2);
+
+const subArray3 = testeSlice.slice(2);//Aqui vai mostrar A PARTIR  de 2
+console.log(subArray3);
+
+// 15 - forEach
+const nums = [1, 2, 3, 4, 5]
+
+nums.forEach((numero) => {
+    console.log(`O número é ${numero}`);
+});
+
+const posts = [
+    {title: "Primeiro post", category: "PHP"},
+    {title: "Segundo post", category: "JavaScript"},
+    {title: "Terceiro post", category: "Python"},
+];
+
+posts.forEach((post) => {
+    console.log(`Exibindo post: ${post.title}, da categoria: ${post.category}`);
+});
+
+// 16 - includes
+const brands = ["BMW", "VW", "Fiat"]
+
+console.log(brands.includes("Fiat"));
+console.log(brands.includes("KIA"));//Aqui ele retorna um booleano, não um indexOf
+
+if(brands.includes("BMW")) {
+    console.log("Há carros da marca BMW!");
+}
